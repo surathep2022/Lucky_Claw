4fbf0c30e81192b4acc73ba6004e34ba04940cd1
Surathep <surathep2022@gmail.com>
2026-09-10 20:16:53 +0700

    edit stock item

--- script.js
+++ script.js
@@ -133,9 +133,9 @@
             // { name: "เครื่องผลไม้ปั่น", image: "gift/15.png" }, 
             // { name: "เครื่องปั้นน้ำผลไม้", image: "gift/14.png" },
             // { name: "เครื่องพ่นไอน้ำ" , image: "gift/17.png" },
-             { name: "เครื่องจำกัดไรฝุ่น", image: "gift/18.png"},
+            // { name: "เครื่องจำกัดไรฝุ่น", image: "gift/18.png"},
             // { name: "เครื่องบดเนื้อ", image: "gift/19.png" },
-            // { name: "เครื่องเตรียมอาหาร", image: "gift/12.png" },
+            { name: "เครื่องเตรียมอาหาร", image: "gift/12.png" },
             // { name: "หม้อทอดไร้น้ำมัน", image: "gift/16.png" },
             // { name: "เครื่องดูดฝุ่น", image: "gift/13.png" },  
             
